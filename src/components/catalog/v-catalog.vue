d21474a61c5e33f0f1561a046b0efc0a3ca2451a
Kostya <Kpstya@mail.ru>
2020-11-08 22:44:13 +0300

    added new select

--- src/components/catalog/v-catalog.vue
+++ src/components/catalog/v-catalog.vue
@@ -1,5 +1,11 @@
 <template>
   <div class="v-catalog">
+    <v-select
+        :categories="categories"
+        @select="sortByCategories"
+        :selected="selected"
+    >
+    </v-select>
     <v-textfield
         v-model="model"
         label="Placeholder"
@@ -12,7 +18,7 @@
     <h1>{{title}}</h1>
     <div class="v-catalog__list">
       <v-catalog-item
-          v-for="product in PRODUCTS"
+          v-for="product in filteredProducts"
           :key="product.article"
           :product_data="product"
           @addToCart="addToCart"
@@ -25,19 +31,28 @@
 <script>
 import vCatalogItem from './v-catalog-item'
 import vTextfield from '../v-textfield'
+import vSelect from '../v-select'
 
 import {mapActions, mapGetters} from 'vuex'
 export default {
 name: "v-catalog",
   components:{
     vCatalogItem,
-    vTextfield
+    vTextfield,
+    vSelect
   },
   props:{},
   data(){
     return{
       title: 'Catalog',
-      model: ''
+      model: '',
+      categories:[
+        {name: 'all clothes', value: 'all'},
+        {name: "man's clothing", value: 'men'},
+        {name: "women's clothing", value: 'women'},
+      ],
+      selected: 'All clothes',
+      sortedProducts: []
     }
   },
   computed:{
@@ -45,6 +60,13 @@ name: "v-catalog",
         'PRODUCTS',
         'CART'
     ]),
+    filteredProducts(){
+      if(this.sortedProducts.length){
+        return this.sortedProducts
+      }else{
+        return this.PRODUCTS
+      }
+    }
   },
   methods:{
     ...mapActions([
@@ -53,6 +75,16 @@ name: "v-catalog",
     ]),
     addToCart(data){
         this.ADD_TO_CART(data)
+    },
+    sortByCategories(category){
+      this.sortedProducts = [];
+      let vm = this;
+      this.PRODUCTS.map(function (item){
+        if(item.category === category.name){
+          vm.sortedProducts.push(item)
+        }
+      })
+      this.selected = category.name
     }
   },
   mounted() {
---
+++ src/components/v-select.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="v-select">
+    <p class="select btn"
+       @click="areOptionsVisible = !areOptionsVisible"
+    >{{ selected }}</p>
+    <div class="categories"
+         v-if="areOptionsVisible"
+    >
+      <div class="category"
+        v-for="category in categories"
+        :key="category.value"
+        @click="selectOption(category)"
+      >
+        {{category.name}}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "v-select",
+  data(){
+    return{
+      areOptionsVisible: false
+    }
+  },
+  props:{
+    categories:{
+      type: Array,
+      default(){
+        return []
+      }
+    },
+    selected:{
+      type: String,
+      default: ''
+    }
+  },
+  computed:{
+
+  },
+ // mounted(){
+  //  document.addEventListener('click', this.hideSelect.bind(this), true)
+  //},
+ // beforeDestroy() {
+  //  document.removeEventListener('click', this.hideSelect)
+ // },
+  methods:{
+    selectOption(category){
+      this.$emit('select', category)
+      this.areOptionsVisible = false
+    },
+    //hideSelect(){
+    //  this.areOptionsVisible = false
+    //}
+  }
+}
+</script>
+
+<style>
+  .v-select{
+    position: relative;
+  }
+  .select{
+    width: 200px;
+  }
+
+</style>
